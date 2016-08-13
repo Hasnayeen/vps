0490d2d6c33bc0506f6a8cf012b05cd48e93c7e1
nhasnayeen <nehal.hasnayeen@gagagugu.com>
2016-08-13 15:50:42 +0600

    initial setup

---
+++ src/Providers/DeployersServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Iluminar\Deployer\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class DeployerServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        
+    }
+}
