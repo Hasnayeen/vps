ba83bfd2017bb6742bd35fb6cf171f485e4dbb55
Nehal Hasnayeen <searching.nehal@gmail.com>
2016-08-19 15:13:51 +0600

    domain class added and domain record methods added

---
+++ src/Core/Domain.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Iluminar\VPS\Core;
+
+use Iluminar\VPS\Core\VPS;
+
+class Domain extends VPS
+{
+    protected $endpoint = 'domains';
+    protected $id;
+
+    function __construct($id)
+    {
+        parent::setHeader();
+        $this->id = $id;
+    }
+
+    public function records()
+    {
+        return Request::get($this->endpoint . '/' . $this->id . '/' . __FUNCTION__, $this->header);
+    }
+
+    public function record()
+    {
+        $this->endpoint .= $this->id . '/records';
+
+        return $this;
+    }
+
+    public function create($param)
+    {
+        return Request::post($this->endpoint, ['json' => $params] + $this->header);
+    }
+
+    public function get($id)
+    {
+        return Request::get($this->endpoint . '/' . $this->id, $this->header);
+    }
+
+    public function update($id, $params)
+    {
+        return Request::put($this->endpoint . '/' . $this->id, ['json' => $params] + $this->header);
+    }
+
+    public function delete($id)
+    {
+        return Request::delete($this->endpoint . '/' . $this->id, $this->header);
+    }
+}
